814502319b11dbd3c80a107c2aa81e813e0c4db4
Fly007 <Fly007>
2019-01-21 11:54:22 +0800

    文件优化，src里无用的css删掉，给项目瘦身

--- src/router.js
+++ src/router.js
@@ -6,16 +6,25 @@ Vue.use(Router);
 
 	// import list from "./components/list";
 	const list = () => import( './components/list')
-
-	import buycart from "./components/buycart";
-	import count from "./components/count";
-	import finding from "./components/finding";
-	import productlist from "./components/productlist";
-	import item from "./components/item";
-	import user from "./components/user";
-	import login from "./components/login";
-	import reg from "./components/reg";
-	import error from "./components/error";
+	const buycart = () => import( './components/buycart')
+	const count = () => import( './components/count')
+	const finding = () => import( './components/finding')
+	const productlist = () => import( './components/productlist')
+	const item = () => import( './components/item')
+	const user = () => import( './components/user')
+	const login = () => import( './components/login')
+	const reg = () => import( './components/reg')
+	const error = () => import( './components/error')
+	
+// 	import buycart from "./components/buycart";
+// 	import count from "./components/count";
+// 	import finding from "./components/finding";
+// 	import productlist from "./components/productlist";
+// 	import item from "./components/item";
+// 	import user from "./components/user";
+// 	import login from "./components/login";
+// 	import reg from "./components/reg";
+// 	import error from "./components/error";
 	
 
 const routes =[
